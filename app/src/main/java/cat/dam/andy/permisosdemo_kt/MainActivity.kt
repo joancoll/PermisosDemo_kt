65c23a57a975b6a370de2729239cb97a0bff704c
joancoll <joancoll@inspladelestany.cat>
2024-01-26 11:31:32 +0100

    PermissionManager added

--- app/src/main/java/cat/dam/andy/permisosdemo_kt/MainActivity.kt
+++ app/src/main/java/cat/dam/andy/permisosdemo_kt/MainActivity.kt
@@ -1,47 +1,18 @@
 package cat.dam.andy.permisosdemo_kt
 
 import android.Manifest
-import android.app.AlertDialog
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
+import android.content.Context
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
-
-data class PermissionInfo(
-    val name: String,
-    val permission: String,
-    val grantedMessage: String,
-    val rationale: String
-)
 
 class MainActivity : AppCompatActivity() {
 
-    private val PERMISSION_GRANTED = PackageManager.PERMISSION_GRANTED
-    private lateinit var activityResultLauncher: ActivityResultLauncher<String>
+    private val context: Context = this
+    private var permissionManager = PermissionManager(context)
 
-    private val permissionsList = listOf(
-        PermissionInfo(
-            "Camera Access",
-            Manifest.permission.CAMERA,
-            "Camera Permission granted",
-            "Camera access is required for taking pictures."
-        ),
-        PermissionInfo(
-            "Contacts Access",
-            Manifest.permission.WRITE_CONTACTS,
-            "Contacts Permission granted",
-            "Contact access is required for managing contacts."
-        ),
-        // Afegeix més permisos si és necessari
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,113 +22,59 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initPermissions() {
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
-                if (isGranted) {
-                    showToast("Permission granted")
-                } else {
-                    showToast("Permission denied")
-                    handlePermissionDenied()
-                }
-            }
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun handlePermissionDenied() {
-        val permanentDeniedPermissions = mutableListOf<String>()
-
-        for (permissionInfo in permissionsList) {
-            if (checkSelfPermission(permissionInfo.permission) != PERMISSION_GRANTED && !shouldShowRequestPermissionRationale(
-                    permissionInfo.permission
-                )
-            ) {
-                permanentDeniedPermissions.add(permissionInfo.permission)
-            }
-        }
-
-        if (permanentDeniedPermissions.isNotEmpty()) {
-            showPermanentDeniedDialog(permanentDeniedPermissions)
-        }
-    }
-
-    private fun showPermanentDeniedDialog(permanentDeniedPermissions: List<String>) {
-        val deniedPermissionNames = permissionsList
-            .filter { it.permission in permanentDeniedPermissions }
-            .joinToString(", ") { it.name }
-        AlertDialog.Builder(this)
-            .setTitle("Permission denied")
-            .setMessage("Permissions « $deniedPermissionNames » were permanently denied. You need to go to Permission settings to allow them. Thanks")
-            .setPositiveButton("Go to settings") { _, _ ->
-                goToAppSettings()
-            }
-            .setNegativeButton("Cancel", null)
-            .create()
-            .show()
-    }
-
-    private fun goToAppSettings() {
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-        intent.data = Uri.parse("package:$packageName")
-
-        // Si estem en Android 11 o versions posteriors, obrim la configuració de l'aplicació directament
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            intent.action = Settings.ACTION_APP_USAGE_SETTINGS
-        }
-
-        startActivity(intent)
+        permissionManager.addPermission(
+            Manifest.permission.CAMERA,
+            getString(R.string.cameraPermissionInfo),
+            getString(R.string.cameraPermissionNeeded),
+            getString(R.string.cameraPermissionDenied),
+            getString(R.string.cameraPermissionThanks),
+            getString(R.string.cameraPermissionSettings)
+        )
+        permissionManager.addPermission(
+            Manifest.permission.WRITE_CONTACTS,
+            getString(R.string.contactsPermissionInfo),
+            getString(R.string.contactsPermissionNeeded),
+            getString(R.string.contactsPermissionDenied),
+            getString(R.string.contactsPermissionThanks),
+            getString(R.string.contactsPermissionSettings)
+        )
     }
 
     private fun initListeners() {
         val llButtonsContainer = findViewById<LinearLayout>(R.id.ll_buttons_container)
-        for (permissionInfo in permissionsList) {
-            val button = createButton(permissionInfo)
-            llButtonsContainer.addView(button)
-            // Afegir un marge entre els botons (exemple: 16dp)
-            val marginInDp = 16
-            val marginInPx = (marginInDp * resources.displayMetrics.density).toInt()
-            val layoutParams = button.layoutParams as LinearLayout.LayoutParams
-            layoutParams.topMargin = marginInPx
+        for (permissionData in permissionManager.getAllNeededPermissions()) {
+            if (permissionData.permission != null && permissionData.permissionInfo != null) {
+                val button = createButton(
+                    permissionData!!,
+                    permissionData.permissionInfo!!
+                )
+                llButtonsContainer.addView(button)
+                // Afegir un marge entre els botons (exemple: 16dp)
+                val marginInDp = 16
+                val marginInPx = (marginInDp * resources.displayMetrics.density).toInt()
+                val layoutParams = button.layoutParams as LinearLayout.LayoutParams
+                layoutParams.topMargin = marginInPx
+            }
         }
     }
 
-    private fun createButton(permissionInfo: PermissionInfo): Button {
+    private fun createButton(permissionData:PermissionManager.PermissionData, permissionInfo:String): Button {
         val button = Button(this)
-        button.text = permissionInfo.name
+        button.text = permissionInfo
         button.setOnClickListener {
-            handlePermissionButton(permissionInfo)
+            if (!permissionManager.hasPermission(permissionData.permission!!)) {
+                permissionManager.askOnePermission(permissionData)
+            }
+            else {
+                //Toast per indicar que ja té el permís
+                Toast.makeText(
+                    context, permissionData.permissionGrantedMessage,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
         // Pots personalitzar més propietats del botó segons les teves necessitats
         return button
     }
 
-    private fun handlePermissionButton(permissionInfo: PermissionInfo) {
-        if (checkSelfPermission(permissionInfo.permission) == PERMISSION_GRANTED) {
-            showToast(permissionInfo.grantedMessage)
-        } else {
-            // Comprova si ja s'ha mostrat la informació de la necessitat del permís
-            if (!shouldShowRequestPermissionRationale(permissionInfo.permission)) {
-                // Si no cal explicació, sol·licita el permís directament
-                activityResultLauncher.launch(permissionInfo.permission)
-            } else {
-                // Altrament, mostra la informació de la necessitat del permís abans de sol·licitar-lo
-                showRationaleDialog(permissionInfo)
-            }
-        }
-    }
-
-    private fun showRationaleDialog(permissionInfo: PermissionInfo) {
-        AlertDialog.Builder(this)
-            .setTitle("Permission required")
-            .setMessage(permissionInfo.rationale)
-            .setPositiveButton("OK") { _, _ ->
-                // Sol·licita el permís després d'informar l'usuari de la necessitat
-                activityResultLauncher.launch(permissionInfo.permission)
-            }
-            .setNegativeButton("Cancel", null)
-            .create()
-            .show()
-    }
 }
---
+++ app/src/main/java/cat/dam/andy/permisosdemo_kt/PermissionManager.kt
@@ -0,0 +1,147 @@
+package cat.dam.andy.permisosdemo_kt
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+class PermissionManager(private val activityContext: Context) {
+    data class PermissionData(
+        var permission: String?,
+        var permissionInfo: String?,
+        var permissionNeededMessage: String?,
+        var permissionDeniedMessage: String?,
+        var permissionGrantedMessage: String?,
+        var permissionPermanentDeniedMessage: String?
+    )
+
+    private val permissionsRequired = mutableListOf<PermissionData>()
+    private var activityResultLauncher: ActivityResultLauncher<Array<String>>? = null
+
+    init {
+        // Inicialitza el launcher per demanar permisos
+        initPermissionLauncher()
+    }
+
+    private fun initPermissionLauncher() {
+        // Inicialitza el launcher per demanar permisos
+        activityResultLauncher =
+            (activityContext as AppCompatActivity).registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions(),
+                ActivityResultCallback<Map<String, Boolean>> { permissions ->
+                    // Check if all permissions are granted
+                    if (permissions.containsValue(false)) {
+                        // Check every permission
+                        for (permissionKey in permissions.keys) {
+                            val position = permissionsRequired.indexOfFirst { it.permission == permissionKey }
+
+                            when {
+                                permissions[permissionKey] == true -> {
+                                    // Permission granted
+                                    showAlert(
+                                        R.string.permissionGranted,
+                                        permissionsRequired[position].permissionGrantedMessage
+                                    )
+                                }
+                                ActivityCompat.shouldShowRequestPermissionRationale(
+                                    activityContext,
+                                    permissionKey
+                                ) -> {
+                                    // Permission denied
+                                    showAlert(
+                                        R.string.permissionDenied,
+                                        permissionsRequired[position].permissionNeededMessage,
+                                        { _, _ ->
+                                            // Ask again for permission
+                                            askOnePermission(permissionsRequired[position])
+                                        },
+                                        { dialogInterface, _ ->
+                                            dialogInterface.dismiss()
+                                        }
+                                    )
+                                }
+                                else -> {
+                                    // Permission denied permanently
+                                    showAlert(
+                                        R.string.permissionPermDenied,
+                                        permissionsRequired[position].permissionPermanentDeniedMessage,
+                                        { _, _ ->
+                                            // Go to app settings
+                                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                            val uri = Uri.fromParts("package", activityContext.packageName, null)
+                                            intent.data = uri
+                                            activityContext.startActivity(intent)
+                                        },
+                                        { dialogInterface, _ ->
+                                            dialogInterface.dismiss()
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            )
+    }
+
+    fun addPermission(permission: String?, permissionInfo: String?, permissionNeededMessage: String, permissionDeniedMessage: String, permissionGrantedMessage: String, permissionPermanentDeniedMessage: String){
+        permissionsRequired.add(PermissionData(permission, permissionInfo, permissionNeededMessage, permissionDeniedMessage, permissionGrantedMessage, permissionPermanentDeniedMessage))
+    }
+
+    fun getAllNeededPermissions(): MutableList<PermissionData> {
+        // Comprova que tingui els permisos necessaris
+        return permissionsRequired
+    }
+
+    fun hasAllNeededPermissions(): Boolean {
+        // Comprova que tingui els permisos necessaris
+        return permissionsRequired.all { hasPermission(it.permission ?: "") }
+    }
+
+    fun getRejectedPermissions(): ArrayList<PermissionData> {
+        // Retorna només els permisos rebutjats
+        return ArrayList(permissionsRequired.filter { !hasPermission(it.permission ?: "") })
+    }
+
+
+    fun hasPermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            activityContext,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun askForPermissions(permissions: ArrayList<PermissionData>) {
+        // Demana tots els permisos necessaris
+        activityResultLauncher?.launch(permissions.map { it.permission ?: "" }.toTypedArray())
+    }
+
+    fun askOnePermission(permission: PermissionData) {
+        // Demana el permís necessari
+        activityResultLauncher?.launch(arrayOf(permission.permission ?: ""))
+    }
+
+    private fun showAlert(
+        titleResId: Int,
+        message: String?,
+        positiveClickListener: DialogInterface.OnClickListener? = null,
+        negativeClickListener: DialogInterface.OnClickListener? = null
+    ) {
+        AlertDialog.Builder(activityContext)
+            .setTitle(titleResId)
+            .setMessage(message)
+            .setCancelable(true)
+            .setPositiveButton("Ok", positiveClickListener)
+            .setNegativeButton("Cancel", negativeClickListener)
+            .create()
+            .show()
+    }
+}
